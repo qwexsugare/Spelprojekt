471bce47e712d787f5d0d81011638ca50785870a
MÖG <m.o.g@live.se>
2012-11-19 15:37:39 +0100

    test2

--- GameClient/main.cpp
+++ GameClient/main.cpp
@@ -2,5 +2,5 @@
 
 int WINAPI wWinMain(HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPWSTR _lpCmdLine, int _nCmdShow)
 {
-	//this is a comment i like beer
+	//this is a comment i like beer new comment
 }
