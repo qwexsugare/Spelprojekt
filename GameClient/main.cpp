3a3409b824164bef33ba53821841085bd6b09352
Gunnar Södergren <gunnar.sodergren@gmail.com>
2012-11-19 16:27:34 +0100

    Added comment
    Duh...

--- GameClient/main.cpp
+++ GameClient/main.cpp
@@ -3,4 +3,5 @@
 int WINAPI wWinMain(HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPWSTR _lpCmdLine, int _nCmdShow)
 {
 	//this is a comment i like beer new comment
+	//this is awesome, I think...
 }
