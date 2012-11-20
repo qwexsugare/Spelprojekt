2beddb2a36a20834f7b399f42970b8dfbaed5b4d
Simon Karlsson <s91k@hotmail.com>
2012-11-20 04:28:49 -0800

    yhu

--- GameClient/main.cpp
+++ GameClient/main.cpp
@@ -4,4 +4,7 @@ int WINAPI wWinMain(HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPWSTR _lpCm
 {
 	//this is a comment i like beer new comment
 	//this is awesome, I think...
+
+	//GITGITGITGIT
+	//lolol
 }
