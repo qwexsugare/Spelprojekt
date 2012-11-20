35f5dc719b2c68e9490231b3375714d18f541d66
MÖG <m.o.g@live.se>
2012-11-20 13:12:51 +0100

    gt

--- GraphicsEngine/GraphicsHandler.h
+++ GraphicsEngine/GraphicsHandler.h
@@ -1,2 +1,17 @@
 #include "GraphicsHandler.h"
 //AFAFAFFAFAFA
+#ifndef GRAPHICS_HANDLER
+#define GRAPHICS_HANDLER
+
+#include "stdafx.h"
+
+class GraphicsHandler
+{
+private:
+
+public:
+	GraphicsHandler();
+	~GraphicsHandler();
+};
+
+#endif
---
+++ GraphicsEngine/stdafx.h
@@ -0,0 +1,7 @@
+#include <Windows.h>
+#include <D3D10.h>
+#include <D3DX10.h>
+
+#include <vector>
+#include <string>
+using namespace std;
