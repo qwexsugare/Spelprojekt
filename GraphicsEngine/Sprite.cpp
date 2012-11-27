0b7d27407a5876737a1ee309a21ab269e2563669
Simon Karlsson <s91k@hotmail.com>
2012-11-27 17:01:32 +0100

    ok

--- GraphicsEngine/Sprite.cpp
+++ GraphicsEngine/Sprite.cpp
@@ -2,65 +2,10 @@
 
 Sprite::Sprite(ID3D10Device *device, D3DXVECTOR2 position, D3DXVECTOR2 size,ID3D10Texture2D *_texture)
 {
-	this->m_texture = _texture;
-	this->m_mesh = new Mesh(NULL, 6);
-
-	//Create the vertex buffer
-	D3D10_BUFFER_DESC bd;
-	bd.Usage = D3D10_USAGE_DYNAMIC;
-	bd.ByteWidth = sizeof( Vertex ) * 6;
-	bd.BindFlags = D3D10_BIND_VERTEX_BUFFER;
-	bd.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
-	bd.MiscFlags = 0;
-
-	HRESULT hr = device->CreateBuffer( &bd, 0, &this->m_mesh->buffer);
-
-	if(FAILED(hr))
-	{
-		MessageBox( 0, "Unable to create Vertex Buffer", "VB Error", 0 );
-	}
-
-	//Load vertices
-	Vertex *vertexData = NULL;
-	this->m_mesh->buffer->Map( D3D10_MAP_WRITE_DISCARD, 0, reinterpret_cast< void** >((void**)&vertexData));
-
-	vertexData[0].pos = D3DXVECTOR3(position.x, position.y, 0.0f);
-	vertexData[0].texCoord = D3DXVECTOR2(0.0f, 0.0f);
-
-	vertexData[1].pos = D3DXVECTOR3(position.x + size.x, position.y, 0.0f);
-	vertexData[1].texCoord = D3DXVECTOR2(1.0f, 0.0f);
-
-	vertexData[2].pos = D3DXVECTOR3(position.x, position.y + size.y, 0.0f);
-	vertexData[2].texCoord = D3DXVECTOR2(0.0f, 1.0f);
-
-	vertexData[3].pos = D3DXVECTOR3(position.x + size.x, position.y + size.y, 0.0f);
-	vertexData[3].texCoord = D3DXVECTOR2(1.0f, 1.0f);
-
-	vertexData[4].pos = D3DXVECTOR3(position.x, position.y + size.y, 0.0f);
-	vertexData[4].texCoord = D3DXVECTOR2(0.0f, 1.0f);
-
-	vertexData[5].pos = D3DXVECTOR3(position.x + size.x, position.y, 0.0f);
-	vertexData[5].texCoord = D3DXVECTOR2(1.0f, 0.0f);
-	
-	this->m_mesh->buffer->Unmap();
+	D3DX10CreateSprite(device, 50, this->m_sprite);
 }
 
 Sprite::~Sprite()
 {
-	delete this->m_mesh;
 
-	if(this->m_texture != NULL)
-	{
-		this->m_texture->Release();
-	}
-}
-
-Mesh *Sprite::getMesh()
-{
-	return this->m_mesh;
-}
-
-ID3D10Texture2D *Sprite::getTexture()
-{
-	return this->m_texture;
 }
--- GraphicsEngine/Sprite.h
+++ GraphicsEngine/Sprite.h
@@ -8,14 +8,12 @@
 class Sprite
 {
 private:
-	Mesh *m_mesh;
-	ID3D10Texture2D *m_texture;
+	LPD3DX10SPRITE *m_sprite;
 public:
 	Sprite(ID3D10Device *device, D3DXVECTOR2 position, D3DXVECTOR2 size,ID3D10Texture2D *_texture);
 	~Sprite();
 
-	Mesh *getMesh();
-	ID3D10Texture2D *getTexture();
+	D3DX10_SPRITE *getSprite();
 
 	void setPosition(D3DXVECTOR2 position);
 	void setSize(D3DXVECTOR2 size);
