50b46ffe66bda448e2ddd1a6096852a8211ba6fd
MÖG <m.o.g@live.se>
2013-03-14 11:02:36 +0100

    Poison turret super duper powers

--- GameClient/PoisonTurretProjectile.h
+++ GameClient/PoisonTurretProjectile.h
@@ -9,12 +9,12 @@ private:
 	unsigned int m_target;
 	unsigned int m_masterOwner;
 	float m_timeToImpact;
-	unsigned int m_mentalDamage;
+	int m_upgradeLevel;
 public:
 	static const float SLOW_EFFECT;
 	static const int VELOCITY = 7.0f;
 
-	PoisonTurretProjectile(unsigned int _master, unsigned int _target, unsigned int _mentalDamage);
+	PoisonTurretProjectile(unsigned int _master, unsigned int _target, int _upgradeLevel);
 	~PoisonTurretProjectile();
 
 	void update(float _dt);
