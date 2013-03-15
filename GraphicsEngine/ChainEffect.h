9dbf09bc2ca6c4a211c70a87273424fd80f9dd0c
Ghazp <caster_444@hotmail.com>
2013-03-15 08:41:32 +0100

    openDoorMaddafacka och chainStrikeStuff

--- GraphicsEngine/ChainEffect.h
+++ GraphicsEngine/ChainEffect.h
@@ -2,21 +2,27 @@
 
 #include "ChainFXEffectFile.h"
 #include "VertexStructs.h"
+#include "TextureHolder.h"
 
 class ChainEffect
 {
 private:
 	D3DXVECTOR3 m_orig;
 	D3DXVECTOR3 m_target;
+	D3DXVECTOR3 m_camPos;
 	D3DXMATRIX m_viewProj;
 	ID3D10Device* m_device;
 	ID3D10Buffer* m_vertexBuffer;
+	ID3D10ShaderResourceView* m_texture;
 	void CreateVertexBuffer();
 public:
-	ChainEffect(ID3D10Device* _device);
+	ChainEffect(ID3D10Device* _device, TextureHolder* _textureHolder);
 	~ChainEffect();
 	void update(float _dt);
 	void draw(ChainFXEffectFile* fxFile);
 	DECLDIR void setViewProj(D3DXMATRIX _viewProj);
+	DECLDIR void setOrig(D3DXVECTOR3 _vec);
+	DECLDIR void setTarget(D3DXVECTOR3 _vec);
+	DECLDIR void setCamPos(D3DXVECTOR3 _vec);
 };
 
--- GraphicsEngine/ChainFXEffectFile.h
+++ GraphicsEngine/ChainFXEffectFile.h
@@ -8,6 +8,7 @@ private:
 	ID3D10EffectVectorVariable* m_orig;
 	ID3D10EffectVectorVariable* m_target;
 	ID3D10EffectMatrixVariable* m_viewProj;
+	ID3D10EffectVectorVariable* m_camPos;
 	ID3D10EffectTechnique *m_technique;
 	ID3D10InputLayout *m_vertexLayout;
 public:
@@ -18,8 +19,9 @@ public:
 	ID3D10EffectTechnique *getTechnique();
 	ID3D10InputLayout* getInputLayout(){return this->m_vertexLayout;}
 
-	void setTexture(ID3D10ShaderResourceView* _texture);
-	void setOrig(D3DXVECTOR3 _vec);
-	void setTarget(D3DXVECTOR3 _vec);
-	void setViewProj(D3DXMATRIX _matrix);
+	DECLDIR void setTexture(ID3D10ShaderResourceView* _texture);
+	DECLDIR void setOrig(D3DXVECTOR3 _vec);
+	DECLDIR void setTarget(D3DXVECTOR3 _vec);
+	DECLDIR void setViewProj(D3DXMATRIX _matrix);
+	DECLDIR void setCamPos(D3DXVECTOR3 _vec);
 };
