9a8da80d593495bcca3af46ef71366b2410875ee
alve <alve_andersson@msn.com>
2013-03-20 15:55:25 +0100

    nu funkar ljud

--- GameClient/WallClientSkillEffect.h
+++ GameClient/WallClientSkillEffect.h
@@ -8,6 +8,8 @@ class WallClientSkillEffect : public ClientSkillEffect
 private:
 	Model *m_model;
 	float m_lifetime;
+	Mesh* m_weapon;
+	UINT m_masterId;
 public:
 	WallClientSkillEffect(unsigned int _masterId, FLOAT3 _position);
 	~WallClientSkillEffect();
