d8cc55ad49ea3f9287b3b1817dafb2b606ed1948
qwexsugare <m.o.g@live.se>
2013-03-22 21:34:00 +0100

    Update Mouse.h

--- GameClient/Mouse.h
+++ GameClient/Mouse.h
@@ -33,7 +33,6 @@ public:
 	void rButtonUp();
 	void setMousePosition(int _x, int _y);
 	void update();
-	void updateCursorPosMoreFrequently();
 	void setMouseVisibility(bool _visible)const;
 
 	Cursor *getCursor();
